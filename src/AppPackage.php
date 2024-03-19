d83ccbb7173c4cfed086749460622ae40bcd704d
Antonio Ianzano <antonio@ianzano.me>
2024-03-19 20:14:36 +0100

    slimming

--- src/AppPackage.php
+++ src/AppPackage.php
@@ -2,15 +2,17 @@
 use Controllers\AppController;
 use Diana\Interfaces\Runnable;
 use Diana\Routing\RoutingPackage;
+use Diana\Runtime\Application;
 use Diana\Runtime\Package;
+use Diana\Support\Debug;
 
 class AppPackage extends Package implements Runnable
 {
-    public function register()
+    public function register(Application $app)
     {
-        $this->registerPackage(RoutingPackage::class);
+        $app->registerPackage(RoutingPackage::class);
 
-        $this->registerController(AppController::class);
+        $app->registerController(AppController::class);
     }
 
     public function boot()
