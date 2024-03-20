0654d33dbf281a4eb442f73444e7855e11ae6864
Antonio Ianzano <antonio@ianzano.me>
2024-03-20 09:40:38 +0100

    Container

--- src/AppPackage.php
+++ src/AppPackage.php
@@ -3,14 +3,14 @@ use Controllers\AppController;
 use Diana\Routing\RoutingPackage;
 use Diana\Runtime\Application;
 use Diana\Runtime\Package;
+use Diana\Support\Debug;
 
 class AppPackage extends Package
 {
-    public function register(Application $app)
+    public function __construct(private Application $app)
     {
-        $app->registerPackage(RoutingPackage::class);
-
-        $app->registerController(AppController::class);
+        $this->app->registerPackage(RoutingPackage::class);
+        $this->app->registerController(AppController::class);
     }
 
     public function boot()
